94b1870c5ef34a1d3fb56161037629bd1313cde1
Ishu-04 <145926433+Ishu-04@users.noreply.github.com>
2025-01-23 13:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = documnet.getElementById("hamburger");
+const navlinks = documnet.getElementById("navlinks");
+
+
+hamburger.addEventListener("click", ()=>{
+      navlinks.classList.toggle("navlinks-active");
+
+
+})
